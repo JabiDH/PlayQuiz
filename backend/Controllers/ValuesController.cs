bfe8595ef00e3677a71d57a8321d4e7577b71596
Gabi Habash <gabih@sedata.com>
2018-11-01 14:05:11 -0400

    Impletements Logging Service using NLog.

--- backend/Controllers/ValuesController.cs
+++ backend/Controllers/ValuesController.cs
@@ -1,44 +1,51 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Services.LoggingService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+  [Route("api/[controller]")]
+  public class ValuesController : Controller
+  {
+    private readonly ILoggerManager Logger;
+    public ValuesController(ILoggerManager logger)
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      this.Logger = logger;
     }
+    // GET api/values
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      Logger.LogInfo("ValuesController -> Get()");
+      return new string[] { "value1", "value2" };
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
 }
